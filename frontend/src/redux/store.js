29ea4ba3a725679a16d2be9a7259ea485cb439a6
madjek <90720831+madjek@users.noreply.github.com>
2022-01-24 20:32:50 +0100

    redux registration login

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,23 @@
+import {applyMiddleware, combineReducers, createStore} from 'redux';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+import {userLoginReducer, userRegisterReducer} from './reducers/userReducer';
+
+const reducer = combineReducers({
+    userRegister: userRegisterReducer,
+    userLogin: userLoginReducer,
+
+})
+
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
+
+const initialState = {
+    userLogin: {userInfo: userInfoFromStorage}
+};
+
+const middleware = [thunk];
+
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+
+export default store;
