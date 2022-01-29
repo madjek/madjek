6f6a93bc9d2b1dc7fdbea05578cf5444e07c81e9
madjek <90720831+madjek@users.noreply.github.com>
2022-01-29 20:07:31 +0100

    admin order list + product list + product create/edit/delete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,19 @@ import MovieRent from './Projects/MovieRent/Containers/MovieRent';
 import Movie from './Projects/MovieRent/Containers/Movie';
 import BuyCredits from './Projects/MovieRent/Containers/BuyCredits';
 
+import ECommerce from './Projects/ECommerce/Containers/ECommerce';
+import ProductDetails from './Projects/ECommerce/Containers/ProductDetails';
+import Cart from './Projects/ECommerce/Containers/Cart';
+import Shipping from './Projects/ECommerce/Containers/Shipping';
+import Order from './Projects/ECommerce/Containers/Order';
+import OrderDetails from './Projects/ECommerce/Containers/OrderDetails';
+import UserList from './Containers/UseList';
+import UserEdit from './Containers/UserEdit';
+import ProductList from './Containers/ProductList';
+import ProductEdit from './Containers/ProductEdit';
+import OrderList from './Containers/OrderList';
+import MovieList from './Containers/MovieList';
+
 function App() {
   return (
     <BrowserRouter>
@@ -28,11 +41,40 @@ function App() {
             <Route path='/login' element={<Login />} />
             <Route path='/profile' element={<Profile />} />
 
+            <Route path='/admin/userlist' element={<UserList />} />
+            <Route path='/admin/user/:id' element={<UserEdit />} />
+            <Route path='/admin/productlist' element={<ProductList />} />
+            <Route
+              path='/admin/productlist/:pageNumber'
+              element={<ProductList />}
+            />
+            <Route path='/admin/product/:id' element={<ProductEdit />} />
+            <Route path='admin/orderlist' element={<OrderList />} />
+            <Route path='/admin/movieList' element={<MovieList />} />
+
             <Route path='/projects/movierent' element={<MovieRent />} />
             <Route path='/projects/movierent/movie' element={<Movie />} />
             <Route
               path='/projects/movierent/buycredits'
               element={<BuyCredits />}
+            />
+
+            <Route path='/projects/ecommerce' element={<ECommerce />} />
+            <Route
+              path='/projects/ecommerce/page/:pageNumber'
+              element={<ECommerce />}
+            />
+            <Route
+              path='/projects/ecommerce/product/:id'
+              element={<ProductDetails />}
+            />
+            <Route path='/projects/ecommerce/cart/:id' element={<Cart />} />
+            <Route path='/projects/ecommerce/cart' element={<Cart />} />
+            <Route path='/projects/ecommerce/shipping' element={<Shipping />} />
+            <Route path='/projects/ecommerce/placeorder' element={<Order />} />
+            <Route
+              path='/projects/ecommerce/order/:id'
+              element={<OrderDetails />}
             />
           </Routes>
         </Container>
---
+++ frontend/src/Containers/ProductEdit.jsx
@@ -0,0 +1,155 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../Components/Message';
+import Loader from '../Components/Loader';
+import FormContainer from '../Components/FormContainer';
+import {
+  listProductDetails,
+  updateProduct,
+} from '../redux/action/ecommerceActions';
+import { PRODUCT_UPDATE_RESET } from '../redux/constants/ecommerce';
+
+const ProductEdit = () => {
+  const history = useNavigate();
+  const { id } = useParams();
+
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState(0);
+  const [image, setImage] = useState('');
+  const [category, setCategory] = useState('');
+  const [countInStock, setCountInStock] = useState(0);
+  const [description, setDescription] = useState('');
+
+  const dispatch = useDispatch();
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+  const productUpdate = useSelector((state) => state.productUpdate);
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = productUpdate;
+
+  useEffect(() => {
+    if (successUpdate) {
+      dispatch({ type: PRODUCT_UPDATE_RESET });
+      history('/admin/productlist');
+    } else {
+      if (!product.name || product._id !== id) {
+        dispatch(listProductDetails(id));
+      } else {
+        setName(product.name);
+        setPrice(product.price);
+        setImage(product.image);
+        setCategory(product.category);
+        setCountInStock(product.countInStock);
+        setDescription(product.description);
+      }
+    }
+  }, [dispatch, history, id, product, successUpdate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(
+      updateProduct({
+        _id: id,
+        name,
+        price,
+        image,
+        category,
+        description,
+        countInStock,
+      })
+    );
+  };
+
+  return (
+    <>
+      <Link to='/admin/productlist' className='btn btn-light my-3'>
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Edit Product</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant='danger'>{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId='name'>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type='name'
+                placeholder='Enter name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='price'>
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type='number'
+                placeholder='Enter price'
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='image'>
+              <Form.Label>Image</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter image url'
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='countInStock'>
+              <Form.Label>Count In Stock</Form.Label>
+              <Form.Control
+                type='number'
+                placeholder='Enter countInStock'
+                value={countInStock}
+                onChange={(e) => setCountInStock(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='category'>
+              <Form.Label>Category</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter category'
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='description'>
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter description'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Button type='submit' variant='primary'>
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  );
+};
+
+export default ProductEdit;
