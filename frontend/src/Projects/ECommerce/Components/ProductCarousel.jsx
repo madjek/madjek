3ca77f1a18cb3dfa301361b88aff9af5593b463b
madjek <90720831+madjek@users.noreply.github.com>
2022-01-29 20:02:08 +0100

    e-commerce project: main page

---
+++ frontend/src/Projects/ECommerce/Components/ProductCarousel.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Carousel, Image } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Loader from '../../../Components/Loader';
+import Message from '../../../Components/Message';
+import { listTopProducts } from '../../../redux/action/ecommerceActions';
+
+const ProductCarousel = () => {
+  const dispatch = useDispatch();
+
+  const productTopRated = useSelector((state) => state.productTopRated);
+  const { loading, error, products } = productTopRated;
+
+  useEffect(() => {
+    dispatch(listTopProducts());
+  }, [dispatch]);
+
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant='danger'>{error}</Message>
+  ) : (
+    <Carousel variant='dark' pause='hover' className='bg-white'>
+      {products.map((product) => (
+        <Carousel.Item key={product._id}>
+          <Link to={`/projects/ecommerce/product/${product._id}`}>
+            <Image
+              className='my-5 pt-5'
+              src={product.image}
+              alt={product.name}
+              fluid
+            />
+            <Carousel.Caption className='carousel-caption'>
+              <h2>
+                {product.name} (${product.price})
+              </h2>
+            </Carousel.Caption>
+          </Link>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
+
+export default ProductCarousel;
---
+++ frontend/src/Projects/ECommerce/Containers/ECommerce.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Row, Col } from 'react-bootstrap';
+import '../css/Ecommerce.css';
+import Product from '../Components/Product';
+import Message from '../../../Components/Message';
+import Loader from '../../../Components/Loader';
+import Paginate from '../../../Components/Paginate';
+import ProductCarousel from '../Components/ProductCarousel';
+import { listProducts } from '../../../redux/action/ecommerceActions';
+const ECommerce = () => {
+  const pageNumber = 1;
+
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products, page, pages } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts(pageNumber));
+  }, [dispatch, pageNumber]);
+
+  return (
+    <div>
+      <ProductCarousel />
+      <h1>Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate pages={pages} page={page} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ECommerce;
